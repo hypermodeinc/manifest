761751bbf81a631247dbc8d136ced253aefa8897
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-05-22 15:33:01 -0700

    add manifest reader (#6)

---
+++ reader/reader.go
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2024 Hypermode, Inc.
+ */
+
+package reader
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+)
+
+type HypermodeManifest struct {
+	Models []Model `json:"models"`
+	Hosts  []Host  `json:"hosts"`
+}
+
+type ModelTask string
+
+const (
+	ClassificationTask ModelTask = "classification"
+	EmbeddingTask      ModelTask = "embedding"
+	GenerationTask     ModelTask = "generation"
+)
+
+type Model struct {
+	Name        string    `json:"name"`
+	Task        ModelTask `json:"task"`
+	SourceModel string    `json:"sourceModel"`
+	Provider    string    `json:"provider"`
+	Host        string    `json:"host"`
+}
+
+type Host struct {
+	Name       string `json:"name"`
+	Endpoint   string `json:"endpoint"`
+	AuthHeader string `json:"authHeader"`
+}
+
+func (m Model) Hash() string {
+	// Concatenate the attributes into a single string
+	data := m.Name + "|" + string(m.Task) + "|" + m.SourceModel + "|" + m.Provider + "|" + m.Host
+
+	// Compute the SHA-256 hash
+	hash := sha256.Sum256([]byte(data))
+
+	// Convert the hash to a hexadecimal string
+	hashStr := hex.EncodeToString(hash[:])
+
+	return hashStr
+}
+
+func ReadManifest(content []byte) (HypermodeManifest, error) {
+	manifest := HypermodeManifest{}
+	err := json.Unmarshal(content, &manifest)
+	return manifest, err
+}
