36b2319e3c4cd857ed27ec67fc44836ce1f8b784
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-05-22 13:20:27 -0700

    add manifest reader

---
+++ reader/go.mod
@@ -0,0 +1,3 @@
+module github.com/gohypermode/manifest/reader
+
+go 1.22.3
---
+++ reader/reader.go
@@ -0,0 +1,66 @@
+package reader
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type Manifest any
+
+type HypermodeManifest struct {
+	Models               []Model               `json:"models"`
+	Hosts                []Host                `json:"hosts"`
+	EmbeddingSpecs       []EmbeddingSpec       `json:"embeddingSpecs"`
+	TrainingInstructions []TrainingInstruction `json:"trainingInstructions"`
+	Manifest
+}
+
+type ModelTask string
+
+const (
+	ClassificationTask ModelTask = "classification"
+	EmbeddingTask      ModelTask = "embedding"
+	GenerationTask     ModelTask = "generation"
+)
+
+type Model struct {
+	Name        string    `json:"name"`
+	Task        ModelTask `json:"task"`
+	SourceModel string    `json:"sourceModel"`
+	Provider    string    `json:"provider"`
+	Host        string    `json:"host"`
+}
+
+type Host struct {
+	Name       string `json:"name"`
+	Endpoint   string `json:"endpoint"`
+	AuthHeader string `json:"authHeader"`
+}
+
+type EmbeddingSpec struct {
+	EntityType string `json:"entityType"`
+	Attribute  string `json:"attribute"`
+	ModelName  string `json:"modelName"`
+	Config     struct {
+		Query    string `json:"query"`
+		Template string `json:"template"`
+	} `json:"config"`
+}
+
+type TrainingInstruction struct {
+	ModelName string   `json:"modelName"`
+	Labels    []string `json:"labels"`
+}
+
+func (m Model) Hash() string {
+	// Concatenate the attributes into a single string
+	data := m.Name + "|" + string(m.Task) + "|" + m.SourceModel + "|" + m.Provider
+
+	// Compute the SHA-256 hash
+	hash := sha256.Sum256([]byte(data))
+
+	// Convert the hash to a hexadecimal string
+	hashStr := hex.EncodeToString(hash[:])
+
+	return hashStr
+}
