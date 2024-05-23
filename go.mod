4c397dcb1ce8ea105532a655257cf29da8a97a55
Matt Johnson-Pint <mjp@hypermode.com>
2024-05-23 14:59:13 -0700

    Schema Changes (#10)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hypermodeAI/manifest
 
 go 1.22
 
-require github.com/tailscale/hujson v0.0.0-20221223112325-20486734a56a
+require (
+	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1
+	github.com/tailscale/hujson v0.0.0-20221223112325-20486734a56a
+)
--- manifest.go
+++ manifest.go
@@ -6,15 +6,21 @@ package manifest
 
 import (
 	"crypto/sha256"
+	_ "embed"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 
+	"github.com/santhosh-tekuri/jsonschema/v5"
 	"github.com/tailscale/hujson"
 )
 
+//go:embed hypermode.json
+var schemaContent string
+
 type HypermodeManifest struct {
-	Models []ModelInfo `json:"models"`
-	Hosts  []HostInfo  `json:"hosts"`
+	Models map[string]ModelInfo `json:"models"`
+	Hosts  map[string]HostInfo  `json:"hosts"`
 }
 
 type ModelTask string
@@ -26,7 +32,7 @@ const (
 )
 
 type ModelInfo struct {
-	Name        string    `json:"name"`
+	Name        string    `json:"-"`
 	Task        ModelTask `json:"task"`
 	SourceModel string    `json:"sourceModel"`
 	Provider    string    `json:"provider"`
@@ -34,9 +40,11 @@ type ModelInfo struct {
 }
 
 type HostInfo struct {
-	Name       string `json:"name"`
-	Endpoint   string `json:"endpoint"`
-	AuthHeader string `json:"authHeader"`
+	Name            string            `json:"-"`
+	Endpoint        string            `json:"endpoint"`
+	BaseURL         string            `json:"baseURL"`
+	Headers         map[string]string `json:"headers"`
+	QueryParameters map[string]string `json:"queryParameters"`
 }
 
 func (m ModelInfo) Hash() string {
@@ -54,20 +62,29 @@ func (m ModelInfo) Hash() string {
 
 func ReadManifest(content []byte) (HypermodeManifest, error) {
 
-	// We allow comments and trailing commas in the JSON files.
-	// This removes them, resulting in standard JSON.
-	bytes, err := standardizeJSON(content)
+	data, err := standardizeJSON(content)
 	if err != nil {
 		return HypermodeManifest{}, err
 	}
 
-	// Now parse the JSON
+	// Parse the JSON
 	manifest := HypermodeManifest{}
-	err = json.Unmarshal(bytes, &manifest)
+	err = json.Unmarshal(data, &manifest)
+
+	// Copy map keys to Name fields
+	for key, model := range manifest.Models {
+		model.Name = key
+		manifest.Models[key] = model
+	}
+	for key, host := range manifest.Hosts {
+		host.Name = key
+		manifest.Hosts[key] = host
+	}
 
 	return manifest, err
 }
 
+// standardizeJSON removes comments and trailing commas to make the JSON valid
 func standardizeJSON(b []byte) ([]byte, error) {
 	ast, err := hujson.Parse(b)
 	if err != nil {
@@ -75,4 +92,29 @@ func standardizeJSON(b []byte) ([]byte, error) {
 	}
 	ast.Standardize()
 	return ast.Pack(), nil
+}
+
+func ValidateManifest(content []byte) error {
+	sch, err := jsonschema.CompileString("hypermode.json", schemaContent)
+	if err != nil {
+		return err
+	}
+
+	content, err = standardizeJSON(content)
+	if err != nil {
+		return fmt.Errorf("failed to standardize manifest: %w", err)
+	}
+
+	var v interface{}
+	err = json.Unmarshal(content, &v)
+	if err != nil {
+		return fmt.Errorf("failed to deserialize manifest: %w", err)
+	}
+
+	err = sch.Validate(v)
+	if err != nil {
+		return fmt.Errorf("failed to validate manifest: %w", err)
+	}
+
+	return nil
 }
