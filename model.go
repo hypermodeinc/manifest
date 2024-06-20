d198d9ef6540900ee1efe3645e691f01d8397b02
Aman Mangal <aman@hypermode.com>
2024-06-20 12:16:50 +0530

    refactor code to support more host types (#18)

---
+++ model.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 Hypermode, Inc.
+ */
+
+package manifest
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+type ModelInfo struct {
+	Name        string `json:"-"`
+	SourceModel string `json:"sourceModel"`
+	Provider    string `json:"provider"`
+	Host        string `json:"host"`
+	Path        string `json:"path"`
+}
+
+func (m ModelInfo) Hash() string {
+	// Concatenate the attributes into a single string
+	data := fmt.Sprintf("%v|%v|%v|%v", m.Name, m.SourceModel, m.Provider, m.Host)
+
+	// Compute the SHA-256 hash
+	hash := sha256.Sum256([]byte(data))
+
+	// Convert the hash to a hexadecimal string
+	hashStr := hex.EncodeToString(hash[:])
+
+	return hashStr
+}
